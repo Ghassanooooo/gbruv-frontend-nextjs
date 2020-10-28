b6b291a2aae7187be2c82dfef3b12c2e2adee838
Ghassan Aldarwish <ghassanaldarwish@Ghassans-MacBook-Pro.local>
2020-10-28 18:23:21 +0100

    vercel.json

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   publicRuntimeConfig: {
+    PROJECT_ROOT: __dirname,
     localeSubpaths: typeof process.env.LOCALE_SUBPATHS === 'string' ? process.env.LOCALE_SUBPATHS : 'all',
   },
 };
